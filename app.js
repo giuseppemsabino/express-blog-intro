01c544baac85efba41e88955a557bcf5bf050ad5
giuseppemsabino <giuseppemendoza004@gmail.com>
2024-11-19 17:30:59 +0100

    post created and use to with dirname

--- app.js
+++ app.js
@@ -1,15 +1,51 @@
 const express = require('express');
 const app = express();
+const path = require('path');
+
 const port = 3000;
 
 // console.log(app);
 
+const post =[
+    {
+        title:"ciambellone",
+        content:"questa e la foto di un ciambellone",
+        image:"/ciambellone.jpeg",
+        tags:["tutorial","esemio"]
+    },
+    {
+        title:"craker",
+        content:"questa e una foto di crakers di barbabietola",
+        image:"/cracker_barbabietola.jpeg",
+        tags:["introduzione", "benvenuto"]
+    },
+    {
+        title:"pane fritto",
+        content:"questa e la imagine di un pane fritto",
+        image:"/pane_fritto_dolce.jpeg",
+        tags:["novità", "aggiornamento"]
+    },
+    {
+        title:"pasta barbabietola",
+        content:"questa e l'imagine di pasta di barbabietola",
+        image:"/pasta_barbabietola.jpeg",
+        tags:["evento", "annuncio"]
+    },
+    {
+        title:"torta paesana",
+        content:"questa e l'imagine di una torta paesana",
+        image:"/torta_paesana.jpeg",
+        tags: ["conclusione", "finale"]
+    }
+];
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get ('/', (req,res) => {
     res.send('hello wolrd!')
 })
 
 app.listen(port, () => {
-    console.log(`exaple app listening on port ${port}`);
+    console.log(`example app listening on port ${port}`);
     
 })
