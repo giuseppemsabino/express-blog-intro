ffda23bb27e3b744e1c665a469eb7c4d48b725f0
giuseppemsabino <giuseppemendoza004@gmail.com>
2024-11-19 16:49:59 +0100

    first app

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
 const express = require('express');
+const app = express();
+const port = 3000;
+
+// console.log(app);
+
+
+app.get ('/', (req,res) => {
+    res.send('hello wolrd!')
+})
+
+app.listen(port, () => {
+    console.log(`exaple app listening on port ${port}`);
+    
+})
